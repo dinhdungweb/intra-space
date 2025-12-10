9db481e6b1b3bdec51a9bc056a0bfb974498a5a4
dinhdungweb <dungmaster7@gmail.com>
2025-12-10 18:02:41 +0700

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+import path from "path";
+
+const nextConfig: NextConfig = {
+  // reactStrictMode: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname), path.join(__dirname, "node_modules"), path.join(__dirname, "node_modules/bootstrap/scss")],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
